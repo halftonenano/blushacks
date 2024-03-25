0492269524399c78ab0c64c118df9a6420cb5433
halftonenano <86031051+halftonenano@users.noreply.github.com>
2024-03-24 19:37:46 -0700

    deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
-import { treaty } from '@elysiajs/eden'
-import type { App } from '../routes/api/[...path]/+server'
+import { treaty } from '@elysiajs/eden';
+import type { App } from '../routes/api/[...path]/+server';
+import { PUBLIC_URL } from '$env/static/public';
 
-export const client = treaty<App>('localhost:5173');
+export const client = treaty<App>(PUBLIC_URL);
